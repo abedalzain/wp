2dad92fee92a3f250221be698f6a3e45b0bf012f
Abed Alzain <abed.m.alzain@gmail.com>
2015-06-28 12:13:09 +0300

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('ingredientApp.config', [])
+.constant('WORDPRESS_API_URL', 'http://ingredientapp.info/wp/index.php/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://ingredientapp.info/wp/index.php/api/')
+.constant('GCM_SENDER_ID', '230384825625')
+
+;
---
+++ www/js/filters.js
@@ -0,0 +1,7 @@
+angular.module('ingredientApp.filters', [])
+
+.filter('rawHtml', ['$sce', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
+}]);
